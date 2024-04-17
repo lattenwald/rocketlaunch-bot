6aa4a109f216c3c3d45ff08d8dcf4b58ba1edbd2
Alexander Q <qalexx@gmail.com>
2024-04-17 17:35:07 +0300

    WIP

--- src/bot.rs
+++ src/bot.rs
@@ -7,6 +7,7 @@ use teloxide::{
     utils::{command::BotCommands, markdown},
     Bot,
 };
+use tracing::info;
 
 use crate::{config::BotConfig, db::Db};
 
@@ -23,17 +24,17 @@ pub async fn init_bot(config: BotConfig, db: Db) -> (MyBot, MyDispatcher) {
 
     let handler = Update::filter_message()
         .branch(
-            dptree::entry()
-                .filter_command::<UnauthorizedCommand>()
-                .endpoint(unauthorized_command_handler),
-        )
-        .branch(
             dptree::filter(|cfg: BotConfig, msg: Message| cfg.admin_chats.contains(&msg.chat.id.0))
                 .branch(
                     dptree::entry()
                         .filter_command::<Command>()
                         .endpoint(command_handler),
                 ),
+        )
+        .branch(
+            dptree::entry()
+                .filter_command::<UnauthorizedCommand>()
+                .endpoint(unauthorized_command_handler),
         );
 
     (
@@ -45,55 +46,48 @@ pub async fn init_bot(config: BotConfig, db: Db) -> (MyBot, MyDispatcher) {
 }
 
 #[derive(BotCommands, Clone, Debug)]
-#[command(rename_rule = "snake_case", description = "Доступные команды:")]
+#[command(rename_rule = "snake_case", description = "*Команды:*")]
 enum UnauthorizedCommand {
-    #[command(description = "id текущего чата")]
-    Id,
-}
-
-#[derive(BotCommands, Clone, Debug)]
-#[command(rename_rule = "snake_case", description = "Доступные команды:")]
-enum Command {
-    #[command(description = "помощь")]
-    Help,
-
     #[command(description = "id текущего чата")]
     Id,
 
     #[command(description = "подписаться на запуски")]
     Subscribe,
-    // #[command(description = "отписаться от запусков")]
-    // Unsubscribe,
+
+    #[command(description = "отписаться от запусков")]
+    Unsubscribe,
+
+    #[command(description = "помощь")]
+    Help,
 }
 
+#[derive(BotCommands, Clone, Debug)]
+#[command(rename_rule = "snake_case", description = "*Админские команды:*")]
+enum Command {
+    #[command(description = "помощь")]
+    Help,
+}
+
+#[tracing::instrument(skip_all)]
 async fn unauthorized_command_handler(
     bot: MyBot,
     msg: Message,
     cmd: UnauthorizedCommand,
+    db: Db,
 ) -> ResponseResult<()> {
+    info!("handling: {:?}", cmd);
     match cmd {
         UnauthorizedCommand::Id => {
             bot.send_message(msg.chat.id, format!("`{}`", msg.chat.id))
                 .reply_to_message_id(msg.id)
                 .await?;
         }
-    }
-    Ok(())
-}
-
-async fn command_handler(bot: MyBot, msg: Message, cmd: Command, db: Db) -> ResponseResult<()> {
-    match cmd {
-        Command::Help => {
-            bot.send_message(msg.chat.id, Command::descriptions().to_string())
+        UnauthorizedCommand::Help => {
+            bot.send_message(msg.chat.id, UnauthorizedCommand::descriptions().to_string())
                 .reply_to_message_id(msg.id)
                 .await?;
         }
-        Command::Id => {
-            bot.send_message(msg.chat.id, format!("`{}`", msg.chat.id))
-                .reply_to_message_id(msg.id)
-                .await?;
-        }
-        Command::Subscribe => match db.subscribe(msg.chat.id.0) {
+        UnauthorizedCommand::Subscribe => match db.subscribe(msg.chat.id.0) {
             Ok(_) => {
                 bot.send_message(msg.chat.id, "Есть подписка, ждите уведомлений!")
                     .reply_to_message_id(msg.id)
@@ -111,6 +105,41 @@ async fn command_handler(bot: MyBot, msg: Message, cmd: Command, db: Db) -> Resp
                 .await?;
             }
         },
+        UnauthorizedCommand::Unsubscribe => match db.unsubscribe(msg.chat.id.0) {
+            Ok(_) => {
+                bot.send_message(msg.chat.id, "Отписались")
+                    .reply_to_message_id(msg.id)
+                    .await?;
+            }
+            Err(err) => {
+                bot.send_message(
+                    msg.chat.id,
+                    format!(
+                        "Ошибка отписки:\n```\n{}\n```",
+                        markdown::escape(&format!("{:?}", err))
+                    ),
+                )
+                .reply_to_message_id(msg.id)
+                .await?;
+            }
+        },
+    }
+    Ok(())
+}
+
+#[tracing::instrument(skip_all)]
+async fn command_handler(bot: MyBot, msg: Message, cmd: Command) -> ResponseResult<()> {
+    info!("handling: {:?}", cmd);
+    match cmd {
+        Command::Help => {
+            let commands = [
+                Command::descriptions().to_string(),
+                UnauthorizedCommand::descriptions().to_string(),
+            ];
+            bot.send_message(msg.chat.id, commands.join("\n\n"))
+                .reply_to_message_id(msg.id)
+                .await?;
+        }
     }
     Ok(())
 }
